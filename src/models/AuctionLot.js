e9dacbcd2655f65c459e3be14ac5c85bbecbe2c1
unknown <eleonora.nesterovych@nure.ua>
2026-04-07 13:58:47 +0300

    fix: перенос изменений из архива в ветку feature

--- src/models/AuctionLot.js
+++ src/models/AuctionLot.js
@@ -17,12 +17,12 @@ export const copyLot = (lot, newId) => {
 
 // Задание 1 и 3: Создаем 6 объектов
 export const lotsData = [
-  createAuctionLot({ id: 1, title: "Старинные часы", category: "Антиквариат", startPrice: 500 }),
-  createAuctionLot({ id: 2, title: "Картина маслом", category: "Искусство", startPrice: 1200 }),
-  createAuctionLot({ id: 3, title: "Бронзовая статуэтка", category: "Скульптура", startPrice: 300 }),
-  createAuctionLot({ id: 4, title: "Редкая монета", category: "Нумизматика", startPrice: 800 }),
-  createAuctionLot({ id: 5, title: "Фарфоровая ваза", category: "Керамика", startPrice: 150 }),
-  createAuctionLot({ id: 6, title: "Рыцарский меч", category: "Оружие", startPrice: 2000 })
+  createAuctionLot({ id: 1, title: "Старовинний годинник", category: "Антикваріат", startPrice: 500, imageUrl: "https://ireland.apollo.olxcdn.com/v1/files/5gn200ezsyq02-UA/image"}),
+  createAuctionLot({ id: 2, title: "Олійний живопис", category: "Мистецтво", startPrice: 1200, imageUrl: "https://i.pinimg.com/736x/c5/77/6e/c5776ee0cdb51f697d81c9d6717333cd.jpg" }),
+  createAuctionLot({ id: 3, title: "Бронзова статуетка", category: "Скульптура", startPrice: 300, imageUrl: "https://ireland.apollo.olxcdn.com/v1/files/b6nsea75u9ql1-UA/image;s=4272x2848" }),
+  createAuctionLot({ id: 4, title: "Рідкісна монета", category: "Нумізматика", startPrice: 800, imageUrl: "https://ireland.apollo.olxcdn.com/v1/files/qd8ysvreymxn1-UA/image;s=576x1024" }),
+  createAuctionLot({ id: 5, title: "Порцелянова ваза", category: "Кераміка", startPrice: 150, imageUrl: "https://image-thumbs.shafastatic.net/2184589706_310_430" }),
+  createAuctionLot({ id: 6, title: "Лицарський меч", category: "Зброя", startPrice: 2000, imageUrl: "https://fama.ua/uploads/catalog/product/picture/80386/superbig_product.webp" })
 ];
 
 // Задание 4: Створюємо сьомий об’єкт, скопіювавши перший
